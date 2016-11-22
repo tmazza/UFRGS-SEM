4a00de461e9ab3d0bd73737ae95aa1da42eda961
tmazza <tiagomdepaula@gmail.com>
2016-11-22 18:19:27 -0200

    Algoritmo de substituição sem let rec

--- main.fs
+++ main.fs
@@ -62,10 +62,15 @@ let toBool n =
   | TmBool(b) -> b
   | _         -> false
 
-// { v/x } e | TODO
+// { v/x } e
 let rec subFree v x e =
   match e with 
+  | TmIf( e1,e2,e3 ) -> TmIf(subFree v x e1,subFree v x e2,subFree v x e3)
   | TmOp( e1,e2,op ) -> TmOp(subFree v x e1,subFree v x e2, op)
+  | TmApp( e1,e2 ) -> TmApp(subFree v x e1,subFree v x e2)
+  | TmFunc( y,e1 ) when not(x = (TmIdent y)) -> TmFunc( y,subFree v x e1)
+  | TmLet( y,e1,e2 ) when x = (TmIdent y) -> TmLet( y,subFree v x e1, e2)
+  | TmLet( y,e1,e2 ) when not(x = (TmIdent y)) -> TmLet( y,subFree v x e1, subFree v x e2)
   | _ -> if x = e then v else e
 
 (* Implementacao da funcao STEP de avaliacao em um passo *)
@@ -125,56 +130,139 @@ let rec eval t =
   with NoRuleApplies -> t
 
 
-(*TESTES*)
+(* TESTES *)
 let evalList list =
   List.map (fun x-> (fst x,eval (fst x)),(snd x) ) list
 
 // Tuple (input,expected output)
 let testes = [
+  (* TESTES STEP *)
+  
   // (1 + 2) -> (3)
   TmOp(TmNum 1,TmNum 2 ,OpSo),TmNum(3);
+  
   // (-1 + 2) -> (1)
   TmOp(TmNum -1,TmNum 2 ,OpSo),TmNum(1);
+  
   // (1 - 2) -> (-1)
   TmOp(TmNum 1,TmNum 2 ,OpSu),TmNum(-1);
+  
   // (2 * 2) -> (4)
   TmOp(TmNum 2,TmNum 2 ,OpMu),TmNum(4);
+  
   // (4 / 2) -> (2)
   TmOp(TmNum 4,TmNum 2 ,OpDi),TmNum(2);
+  
   // (1 < 2) -> (True)
   TmOp(TmNum 1,TmNum 2 ,OpMe),TmBool(true);
+  
   // (1 <= 2) -> (True)
   TmOp(TmNum 1,TmNum 2 ,OpMei),TmBool(true);
+  
   // (1 == 2) -> (False)
   TmOp(TmNum 1,TmNum 2 ,OpIg),TmBool(false);
+  
   // (2 == 2) -> (True)
   TmOp(TmNum 2,TmNum 2 ,OpIg),TmBool(true);
+  
   // (1 <> 2) -> (True)
   TmOp(TmNum 1,TmNum 2 ,OpDif),TmBool(true);
+  
   // (1 <> 1) -> (False)
   TmOp(TmNum 1,TmNum 1 ,OpDif),TmBool(false);
+  
   // (1 >= 2) -> (False)
   TmOp(TmNum 1,TmNum 2 ,OpMai),TmBool(false);
+  
   // (1 > 2) -> (False)
   TmOp(TmNum 1,TmNum 2 ,OpMa),TmBool(false);
+  
   // (if True then 1-2 else 0) -> (-1)
   TmIf( TmBool(true),TmOp(TmNum 1,TmNum 2 ,OpSu),TmNum(0) ),TmNum(-1);
+  
   // (x) -> (x)
   TmIdent("x"),TmIdent("x");
+  
   // (1) (1 > 2) -> (1) (false)
   TmApp( TmNum(1), TmOp(TmNum 1,TmNum 2 ,OpMa) ),TmApp( TmNum 1, TmBool false );
+  
   // (1 < 2) (1 > 2) -> (true) (false)
   TmApp( TmOp(TmNum 1,TmNum 2 ,OpMe), TmOp(TmNum 1,TmNum 2 ,OpMa) ),TmApp( TmBool true, TmBool false  );
+  
   // (fn x:T => x) (1) -> (1)
   TmApp( TmFunc("x",TmIdent("x")),TmNum 1 ),TmNum 1;
+  
   // (fn x:T => x + 1) (1) -> (2)
   TmApp( TmFunc("x",TmOp(TmIdent("x"),TmNum 1 ,OpSo)),TmNum 1 ),TmNum 2;
+  
   // (fn x:T => x + 1) -> (fn x:T => x + 1)
   TmFunc("x",TmOp(TmIdent("x"),TmNum(1) ,OpSo)),TmFunc("x",TmOp(TmIdent("x"),TmNum(1) ,OpSo)); 
+  
   // (let x = 1 in x + 2) -> (3)
   TmLet("x",TmNum(1), TmOp(TmIdent("x"),TmNum(2),OpSo)),TmNum(3);
+  
   // (let x = 4 - 2 in x + 2) -> (4)
   TmLet("x",TmOp(TmNum(4),TmNum(2),OpSu), TmOp(TmIdent("x"),TmNum(2),OpSo)),TmNum(4);
+  
+  (* Testes substituição (subFree) *) // TODO: separar testes de substituição
+  
+  (** Teste: {e/x}n -> n 
+    (fn x:T => 1) (2) **)
+  TmApp( TmFunc("x", TmNum(1)), TmNum(2)),TmNum(1);
+  
+  (** Teste: {e/x}b -> b
+    (fn x:T => True) (2) **)
+  TmApp( TmFunc("x", TmBool(true)), TmNum(2)),TmBool(true);
+  
+  (** Teste: {e/x}(e1 op e2) -> ({e/x}e1 op {e/x}e2)
+    (fn x:T => x + x) (2) **)
+  TmApp( TmFunc("x", TmOp(TmIdent("x"),TmIdent("x"),OpSo) ), TmNum(2)),TmNum(4);
+  
+  (** Teste: {e/x}(if e1 then e2 else e3) -> (if {e/x}e1 then {e/x}e2 else {e/x}e3)
+    (fn x:T => if x > 0 then x - 1 else x + 1 ) (2) **)
+  TmApp( TmFunc("x", TmIf(TmOp(TmIdent("x"),TmNum(0),OpMa),
+                          TmOp(TmIdent("x"),TmNum(1),OpSu),
+                          TmOp(TmIdent("x"),TmNum(1),OpSo)
+  )), TmNum(2)),TmNum(1);
+  
+  (** Teste: {e/x}(e1 e2) -> ({e/x}e1 {e/x}e2)
+    (fn x:T => (x (x-1)) ) (2) **)
+  TmApp( TmFunc("x", TmApp(
+                        TmIdent("x"),
+                        TmOp(TmIdent("x"),TmNum(1),OpSu)
+  )), TmNum(2)),TmApp(TmNum(2),TmNum(1));
+  
+  (** Teste: {e/x}x -> e
+    (fn x:T => x) (2) **)
+  TmApp( TmFunc("x", TmIdent("x")), TmNum(2)),TmNum(2);
+  
+  (** Teste: {e/x}y -> y
+    (fn x:T => y) (2) **)
+  TmApp( TmFunc("x", TmIdent("y")), TmNum(2)),TmIdent("y");
+  
+  (** Teste: {e/x} fn x:T => e' -> {e/x} fn x:T => e' // variável pressa, substituição não é aplica
+    (fn x:T => (fn x:T => x)) (2) **)
+  TmApp( TmFunc("x", TmFunc("x", TmIdent("x"))), TmNum(2)),TmFunc("x", TmIdent("x"));
+  
+  (** Teste: {e/x} fn y:T => e' -> {e/x} fn y:T => {e/x}e'
+    (fn x:T => (fn x:T => y + x)) (2) **)
+  TmApp( TmFunc("x", TmFunc("y",
+                        TmOp(TmIdent("y"),TmIdent("x"),OpSo)
+  )), TmNum(2)),TmFunc("y",TmOp(TmIdent("y"),TmNum(2),OpSo));
+  
+  (** Teste: {e/x}(let x:T => e1 in e2) -> let x:T {e/x}e1 in e2
+    (fn x:T => (let x:T => x + 3 in x)) (2) **) // Valor do x em e2 só é procesado após substituição do x em e1
+  TmApp( TmFunc("x", TmLet("x",TmOp(TmIdent("x"),TmNum(3),OpSo),TmIdent("x"))), TmNum(2)),TmNum(5);
+  
+  (** Teste: {e/x}(let y:T => e1 in e2) -> let y:T {e/x}e1 in {e/x}e2
+    (fn x:T => (let y:T => x + 3 in x)) (2) **) // x é livre em e2
+  TmApp( TmFunc("x", TmLet("y",TmOp(TmIdent("x"),TmNum(3),OpSo),TmIdent("x"))), TmNum(2)),TmNum(2);
+
+  (** TODO Teste: {e/f}(let rec f:T->T=(fn y:T => e1) in e2) -> (let rec f:T->T=(fn y:T => e1) in e2) 
+  **) // f presso, nada é alterado
+  (** TODO Teste: {e/x}(let rec f:T->T=(fn y:T => e1) in e2 -> (let rec f:T->T={e/x}(fn y:T => e1) in {e/x}e2 
+  **) // quado x != y
+  
 ]
   
 let prettyPrint list =

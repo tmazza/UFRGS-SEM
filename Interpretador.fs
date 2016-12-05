374628761a8c3fa76c479a951b35570418bc743e
tmazza <tiagomdepaula@gmail.com>
2016-12-05 10:36:54 -0200

    Correções

--- Interpretador.fs
+++ Interpretador.fs
@@ -2,12 +2,6 @@ namespace Interpretador
 
 module Gramatica =
 
-  type Value = 
-    | TmVar
-    | TmInt
-    | TmBool 
-    | TmFun of Value * Value
-
   type Operacao = 
     | OpSo   // Soma
     | OpSu   // Subtração
@@ -21,7 +15,7 @@ module Gramatica =
     | OpMai  // Maior ou igual
 
   type term =
-    | TmInt of int                      // n
+    | TmNum of int                      // n
     | TmBool of bool                    // b
     | TmOp of term * term * Operacao    // e1 op e2
     | TmIf of term * term * term        // if e1 then e2 else e3
@@ -50,21 +44,20 @@ module Avaliador =
   (* Funcao auxiliar para determinar se um termo e um valor numérico *)
   let isNum t = 
     match t with 
-    | TmInt(n)  -> true 
+    | TmNum(n)  -> true 
     | _ -> false
 
   (* Se termo é ou não um valor *)
   let isValue t = 
     match t with
-    | TmInt(n)  -> true
+    | TmNum(n)  -> true
     | TmBool(n) -> true
-    | TmRaise   -> true
     | TmNil     -> true
     | _ -> false
 
   let toInt n =
     match n with
-    | TmInt(n) -> n
+    | TmNum(n) -> n
     | _        -> 0
 
   let toBool n =
@@ -111,15 +104,15 @@ module Avaliador =
     match t with
       (* Op *)
       | TmOp( TmRaise,e2,op ) -> TmRaise
-      | TmOp( e1,TmRaise,op ) -> TmRaise
+      | TmOp( e1,TmRaise,op ) when isValue e1-> TmRaise
       | TmOp( e1,e2,op ) 
           when isNum e1 && isNum e2 ->
             match op with
-            | OpSo  -> TmInt( (toInt e1) + (toInt e2) )
-            | OpSu  -> TmInt( (toInt e1) - (toInt e2) )
-            | OpMu  -> TmInt( (toInt e1) * (toInt e2) )
+            | OpSo  -> TmNum( (toInt e1) + (toInt e2) )
+            | OpSu  -> TmNum( (toInt e1) - (toInt e2) )
+            | OpMu  -> TmNum( (toInt e1) * (toInt e2) )
             | OpDi when (toInt e2 = 0) -> TmRaise
-            | OpDi  -> TmInt( (toInt e1) / (toInt e2) )
+            | OpDi  -> TmNum( (toInt e1) / (toInt e2) )
             | OpMe  -> TmBool( (toInt e1) <  (toInt e2) )
             | OpMei -> TmBool( (toInt e1) <= (toInt e2) )
             | OpIg  -> TmBool( (toInt e1) =  (toInt e2) )
@@ -139,7 +132,7 @@ module Avaliador =
           let e1' = step e1 in TmIf( e1',e2,e3 )
       
       (* App *)
-      | TmApp( TmFunc(x,e),v ) -> subFree v (TmIdent x) e
+      | TmApp( TmFunc(x,e),e2 ) -> subFree e2 (TmIdent x) e
       | TmApp( TmRaise,e2 ) -> TmRaise
       | TmApp( e1,TmRaise ) when isValue e1 -> TmRaise
       | TmApp( e1,e2 ) when isValue e1 ->
@@ -150,7 +143,6 @@ module Avaliador =
       (* Let *)
       | TmLet( x,TmRaise,e2 ) -> TmRaise
       | TmLet( x,e1,e2 ) when isValue e1 -> subFree e1 (TmIdent x) e2
-      | TmLet( x,e1,TmRaise ) -> TmRaise
       | TmLet( x,e1,e2 ) ->
           let e1' = step e1 in TmLet( x,e1',e2 )
 
@@ -170,7 +162,7 @@ module Avaliador =
       | TmIsEmp( TmCons( e1,e2 ) ) -> TmBool(false)
       | TmHd( TmNil ) -> TmNil
       | TmHd( TmCons( e1,e2 ) ) -> e1
-      | TmTl( TmNil ) -> TmNil
+      | TmTl( TmNil ) -> TmRaise
       | TmTl( TmCons( e1,e2 ) ) when isValue e2 -> e1
       | TmTl( TmCons( e1,e2 ) ) -> TmTl( e2 )
 
@@ -196,109 +188,109 @@ module Testes =
     (* TESTES STEP *)
     
     // (1 + 2) -> (3)
-    TmOp(TmInt 1,TmInt 2 ,OpSo),TmInt(3);
+    TmOp(TmNum 1,TmNum 2 ,OpSo),TmNum(3);
     
     // (-1 + 2) -> (1)
-    TmOp(TmInt -1,TmInt 2 ,OpSo),TmInt(1);
+    TmOp(TmNum -1,TmNum 2 ,OpSo),TmNum(1);
     
     // (1 - 2) -> (-1)
-    TmOp(TmInt 1,TmInt 2 ,OpSu),TmInt(-1);
+    TmOp(TmNum 1,TmNum 2 ,OpSu),TmNum(-1);
     
     // (2 * 2) -> (4)
-    TmOp(TmInt 2,TmInt 2 ,OpMu),TmInt(4);
+    TmOp(TmNum 2,TmNum 2 ,OpMu),TmNum(4);
     
     // (4 / 2) -> (2)
-    TmOp(TmInt 4,TmInt 2 ,OpDi),TmInt(2);
+    TmOp(TmNum 4,TmNum 2 ,OpDi),TmNum(2);
 
     // (4 / 0) -> TmRaise
-    TmOp(TmInt 4,TmInt 0 ,OpDi),TmRaise;
+    TmOp(TmNum 4,TmNum 0 ,OpDi),TmRaise;
     
     // (1 < 2) -> (True)
-    TmOp(TmInt 1,TmInt 2 ,OpMe),TmBool(true);
+    TmOp(TmNum 1,TmNum 2 ,OpMe),TmBool(true);
     
     // (1 <= 2) -> (True)
-    TmOp(TmInt 1,TmInt 2 ,OpMei),TmBool(true);
+    TmOp(TmNum 1,TmNum 2 ,OpMei),TmBool(true);
     
     // (1 == 2) -> (False)
-    TmOp(TmInt 1,TmInt 2 ,OpIg),TmBool(false);
+    TmOp(TmNum 1,TmNum 2 ,OpIg),TmBool(false);
     
     // (2 == 2) -> (True)
-    TmOp(TmInt 2,TmInt 2 ,OpIg),TmBool(true);
+    TmOp(TmNum 2,TmNum 2 ,OpIg),TmBool(true);
     
     // (1 <> 2) -> (True)
-    TmOp(TmInt 1,TmInt 2 ,OpDif),TmBool(true);
+    TmOp(TmNum 1,TmNum 2 ,OpDif),TmBool(true);
     
     // (1 <> 1) -> (False)
-    TmOp(TmInt 1,TmInt 1 ,OpDif),TmBool(false);
+    TmOp(TmNum 1,TmNum 1 ,OpDif),TmBool(false);
     
     // (1 >= 2) -> (False)
-    TmOp(TmInt 1,TmInt 2 ,OpMai),TmBool(false);
+    TmOp(TmNum 1,TmNum 2 ,OpMai),TmBool(false);
     
     // (1 > 2) -> (False)
-    TmOp(TmInt 1,TmInt 2 ,OpMa),TmBool(false);
+    TmOp(TmNum 1,TmNum 2 ,OpMa),TmBool(false);
     
     // (if True then 1-2 else 0) -> (-1)
-    TmIf( TmBool(true),TmOp(TmInt 1,TmInt 2 ,OpSu),TmInt(0) ),TmInt(-1);
+    TmIf( TmBool(true),TmOp(TmNum 1,TmNum 2 ,OpSu),TmNum(0) ),TmNum(-1);
     
     // (x) -> (x)
     TmIdent("x"),TmIdent("x");
     
     // (1) (1 > 2) -> (1) (false)
-    TmApp( TmInt(1), TmOp(TmInt 1,TmInt 2 ,OpMa) ),TmApp( TmInt 1, TmBool false );
+    TmApp( TmNum(1), TmOp(TmNum 1,TmNum 2 ,OpMa) ),TmApp( TmNum 1, TmBool false );
     
     // (1 < 2) (1 > 2) -> (true) (false)
-    TmApp( TmOp(TmInt 1,TmInt 2 ,OpMe), TmOp(TmInt 1,TmInt 2 ,OpMa) ),TmApp( TmBool true, TmBool false  );
+    TmApp( TmOp(TmNum 1,TmNum 2 ,OpMe), TmOp(TmNum 1,TmNum 2 ,OpMa) ),TmApp( TmBool true, TmBool false  );
     
     // (fn x:T => x) (1) -> (1)
-    TmApp( TmFunc("x",TmIdent("x")),TmInt 1 ),TmInt 1;
+    TmApp( TmFunc("x",TmIdent("x")),TmNum 1 ),TmNum 1;
     
     // (fn x:T => x + 1) (1) -> (2)
-    TmApp( TmFunc("x",TmOp(TmIdent("x"),TmInt 1 ,OpSo)),TmInt 1 ),TmInt 2;
+    TmApp( TmFunc("x",TmOp(TmIdent("x"),TmNum 1 ,OpSo)),TmNum 1 ),TmNum 2;
     
     // (fn x:T => x + 1) -> (fn x:T => x + 1)
-    TmFunc("x",TmOp(TmIdent("x"),TmInt(1) ,OpSo)),TmFunc("x",TmOp(TmIdent("x"),TmInt(1) ,OpSo)); 
+    TmFunc("x",TmOp(TmIdent("x"),TmNum(1) ,OpSo)),TmFunc("x",TmOp(TmIdent("x"),TmNum(1) ,OpSo)); 
     
     // (let x = 1 in x + 2) -> (3)
-    TmLet("x",TmInt(1), TmOp(TmIdent("x"),TmInt(2),OpSo)),TmInt(3);
+    TmLet("x",TmNum(1), TmOp(TmIdent("x"),TmNum(2),OpSo)),TmNum(3);
     
     // (let x = 4 - 2 in x + 2) -> (4)
-    TmLet("x",TmOp(TmInt(4),TmInt(2),OpSu), TmOp(TmIdent("x"),TmInt(2),OpSo)),TmInt(4);
+    TmLet("x",TmOp(TmNum(4),TmNum(2),OpSu), TmOp(TmIdent("x"),TmNum(2),OpSo)),TmNum(4);
     
     // let rec simple = fn x => x in simple(1)
-    TmLetRec("simple", TmFunc( "x", TmIdent("x") ),TmApp(TmIdent("simple"),TmInt(1))),TmInt(1);
+    TmLetRec("simple", TmFunc( "x", TmIdent("x") ),TmApp(TmIdent("simple"),TmNum(1))),TmNum(1);
 
     // fatorial n
     // let rec fat = fn x => if x=0 then 1 else x * fat(x-1) in fat(5)
     TmLetRec("fat", TmFunc( "x", TmIf(
-                                TmOp( TmIdent("x"),TmInt(0),OpIg ),
-                                TmInt(1),
+                                TmOp( TmIdent("x"),TmNum(0),OpIg ),
+                                TmNum(1),
                                 TmOp( 
                                   TmIdent("x"),
-                                  TmApp( TmIdent("fat"),TmOp( TmIdent("x"),TmInt(1),OpSu ) ),
+                                  TmApp( TmIdent("fat"),TmOp( TmIdent("x"),TmNum(1),OpSu ) ),
                                   OpMu 
                                 )
-    )),TmApp(TmIdent("fat"),TmInt(5))),TmInt(120);
+    )),TmApp(TmIdent("fat"),TmNum(5))),TmNum(120);
 
     // raise + 1 -> raise
-    TmOp( TmRaise,TmInt(1),OpSo ),TmRaise;
+    TmOp( TmRaise,TmNum(1),OpSo ),TmRaise;
 
     // 1 + raise -> raise
-    TmOp( TmInt(1),TmRaise,OpSo ),TmRaise;
+    TmOp( TmNum(1),TmRaise,OpSo ),TmRaise;
 
     // if raise then 1 else 2 -> raise
-    TmIf( TmRaise,TmInt(1),TmInt(2) ),TmRaise;
+    TmIf( TmRaise,TmNum(1),TmNum(2) ),TmRaise;
 
     // (fn x:T => x) (raise) -> (raise)
     TmApp( TmFunc("x",TmIdent("x")),TmRaise ),TmRaise;
 
     // (raise) (2) -> (raise)
-    TmApp( TmRaise, TmInt(2) ),TmRaise;
+    TmApp( TmRaise, TmNum(2) ),TmRaise;
 
     // (let x = 1 in raise) -> raise
-    TmLet("x",TmInt(1), TmRaise),TmRaise;
+    TmLet("x",TmNum(1), TmRaise),TmRaise;
     
     // (let x = raise in 1) -> raise // x não ocorre em e1
-    TmLet("x",TmRaise, TmInt(1)),TmRaise;
+    TmLet("x",TmRaise, TmNum(1)),TmRaise;
 
     // (let x = raise in x) -> raise // x ocorre em e1
     TmLet("x",TmRaise, TmIdent("x")),TmRaise;
@@ -307,131 +299,131 @@ module Testes =
     TmLetRec("simple", TmFunc( "x", TmIdent("x") ),TmRaise),TmRaise;
 
     // try if true then 1 else 2 with 5 -> 1
-    TmTry( TmIf( TmBool(true),TmInt(1),TmInt(2) ),TmInt(5) ),TmInt(1);
+    TmTry( TmIf( TmBool(true),TmNum(1),TmNum(2) ),TmNum(5) ),TmNum(1);
     
     // try if true then raise else 2 with 5 -> 5
-    TmTry( TmIf( TmBool(true),TmRaise,TmInt(2) ),TmInt(5) ),TmInt(5);
+    TmTry( TmIf( TmBool(true),TmRaise,TmNum(2) ),TmNum(5) ),TmNum(5);
 
     // try 1 else 2 with 5 -> 1
-    TmTry( TmInt(1),TmInt(5) ),TmInt(1);
+    TmTry( TmNum(1),TmNum(5) ),TmNum(1);
 
     // try 1 else 2 with raise -> 1
-    TmTry( TmInt(1),TmRaise ),TmInt(1);
+    TmTry( TmNum(1),TmRaise ),TmNum(1);
 
     // 1::nil -> 1::nil
-    TmCons( TmInt(1),TmNil ),TmCons( TmInt(1),TmNil );
+    TmCons( TmNum(1),TmNil ),TmCons( TmNum(1),TmNil );
 
     // (fn x => x + 1)::1::nil -> (fn x => x + 1)::1::nil
     TmCons( 
-      TmFunc( "x",TmOp( TmIdent("x"),TmInt(1),OpSo ) ), 
-      TmCons( TmInt(1),TmNil )),
-    TmCons( TmFunc( "x",TmOp( TmIdent("x"),TmInt(1),OpSo ) ), TmCons( TmInt(1),TmNil ));
+      TmFunc( "x",TmOp( TmIdent("x"),TmNum(1),OpSo ) ), 
+      TmCons( TmNum(1),TmNil )),
+    TmCons( TmFunc( "x",TmOp( TmIdent("x"),TmNum(1),OpSo ) ), TmCons( TmNum(1),TmNil ));
 
     // isEmpty nil -> true
     TmIsEmp( TmNil ),TmBool(true);
 
     // isEmpty 1::nil -> false
-    TmIsEmp( TmCons( TmInt(1),TmNil ) ),TmBool(false);
+    TmIsEmp( TmCons( TmNum(1),TmNil ) ),TmBool(false);
 
     // isEmpty 1 -> isEmpty 1 (NoRuleApplies)
-    TmIsEmp( TmInt(1) ),TmIsEmp( TmInt(1) );
+    TmIsEmp( TmNum(1) ),TmIsEmp( TmNum(1) );
 
     // hd nil -> nil
     TmHd( TmNil ), TmNil;
 
     // hd 1::nil -> 1
-    TmHd( TmCons( TmInt(1),TmNil ) ), TmInt(1);
+    TmHd( TmCons( TmNum(1),TmNil ) ), TmNum(1);
 
     // hd (fn x => x + 1)::1::nil -> (fn x => x + 1)
-    TmHd( TmCons( TmFunc( "x",TmOp( TmIdent("x"),TmInt(1),OpSo ) ), TmCons( TmInt(1),TmNil )) )
-    ,TmFunc( "x",TmOp( TmIdent("x"),TmInt(1),OpSo ) );
+    TmHd( TmCons( TmFunc( "x",TmOp( TmIdent("x"),TmNum(1),OpSo ) ), TmCons( TmNum(1),TmNil )) )
+    ,TmFunc( "x",TmOp( TmIdent("x"),TmNum(1),OpSo ) );
 
-    // tl nil -> nil
-    TmTl( TmNil ), TmNil;
+    // tl nil -> TmRaise
+    TmTl( TmNil ), TmRaise;
 
     // tl 1::nil -> 1
-    TmTl( TmCons( TmInt(1),TmNil ) ), TmInt(1);
+    TmTl( TmCons( TmNum(1),TmNil ) ), TmNum(1);
 
     // tl (fn x => x + 1)::1::nil -> (fn x => x + 1)
-    TmTl( TmCons( TmFunc( "x",TmOp( TmIdent("x"),TmInt(1),OpSo ) ), TmCons( TmInt(1),TmNil )) )
-    ,TmInt(1);
+    TmTl( TmCons( TmFunc( "x",TmOp( TmIdent("x"),TmNum(1),OpSo ) ), TmCons( TmNum(1),TmNil )) )
+    ,TmNum(1);
 
     // tl 1::2::3::nil -> 3
-    TmTl( TmCons( TmInt(1),TmCons( TmInt(2),TmCons( TmInt(3),TmNil ) ) ) ), TmInt(3);
+    TmTl( TmCons( TmNum(1),TmCons( TmNum(2),TmCons( TmNum(3),TmNil ) ) ) ), TmNum(3);
 
     (******* Testes substituição (subFree) *******) // TODO: separar os testes de substituição
     (** Teste: {e/x}n -> n 
       (fn x:T => 1) (2) **)
-    TmApp( TmFunc("x", TmInt(1)), TmInt(2)),TmInt(1);
+    TmApp( TmFunc("x", TmNum(1)), TmNum(2)),TmNum(1);
     
     (** Teste: {e/x}b -> b
       (fn x:T => True) (2) **)
-    TmApp( TmFunc("x", TmBool(true)), TmInt(2)),TmBool(true);
+    TmApp( TmFunc("x", TmBool(true)), TmNum(2)),TmBool(true);
     
     (** Teste: {e/x}(e1 op e2) -> ({e/x}e1 op {e/x}e2)
       (fn x:T => x + x) (2) **)
-    TmApp( TmFunc("x", TmOp(TmIdent("x"),TmIdent("x"),OpSo) ), TmInt(2)),TmInt(4);
+    TmApp( TmFunc("x", TmOp(TmIdent("x"),TmIdent("x"),OpSo) ), TmNum(2)),TmNum(4);
     
     (** Teste: {e/x}(if e1 then e2 else e3) -> (if {e/x}e1 then {e/x}e2 else {e/x}e3)
       (fn x:T => if x > 0 then x - 1 else x + 1 ) (2) **)
-    TmApp( TmFunc("x", TmIf(TmOp(TmIdent("x"),TmInt(0),OpMa),
-                            TmOp(TmIdent("x"),TmInt(1),OpSu),
-                            TmOp(TmIdent("x"),TmInt(1),OpSo)
-    )), TmInt(2)),TmInt(1);
+    TmApp( TmFunc("x", TmIf(TmOp(TmIdent("x"),TmNum(0),OpMa),
+                            TmOp(TmIdent("x"),TmNum(1),OpSu),
+                            TmOp(TmIdent("x"),TmNum(1),OpSo)
+    )), TmNum(2)),TmNum(1);
     
     (** Teste: {e/x}(e1 e2) -> ({e/x}e1 {e/x}e2)
       (fn x:T => (x (x-1)) ) (2) **)
     TmApp( TmFunc("x", TmApp(
                           TmIdent("x"),
-                          TmOp(TmIdent("x"),TmInt(1),OpSu)
-    )), TmInt(2)),TmApp(TmInt(2),TmInt(1));
+                          TmOp(TmIdent("x"),TmNum(1),OpSu)
+    )), TmNum(2)),TmApp(TmNum(2),TmNum(1));
     
     (** Teste: {e/x}x -> e
       (fn x:T => x) (2) **)
-    TmApp( TmFunc("x", TmIdent("x")), TmInt(2)),TmInt(2);
+    TmApp( TmFunc("x", TmIdent("x")), TmNum(2)),TmNum(2);
     
     (** Teste: {e/x}y -> y
       (fn x:T => y) (2) **)
-    TmApp( TmFunc("x", TmIdent("y")), TmInt(2)),TmIdent("y");
+    TmApp( TmFunc("x", TmIdent("y")), TmNum(2)),TmIdent("y");
     
     (** Teste: {e/x} fn x:T => e' -> {e/x} fn x:T => e' // variável pressa, substituição não é aplica
       (fn x:T => (fn x:T => x)) (2) **)
-    TmApp( TmFunc("x", TmFunc("x", TmIdent("x"))), TmInt(2)),TmFunc("x", TmIdent("x"));
+    TmApp( TmFunc("x", TmFunc("x", TmIdent("x"))), TmNum(2)),TmFunc("x", TmIdent("x"));
     
     (** Teste: {e/x} fn y:T => e' -> {e/x} fn y:T => {e/x}e'
       (fn x:T => (fn x:T => y + x)) (2) **)
     TmApp( TmFunc("x", TmFunc("y",
                           TmOp(TmIdent("y"),TmIdent("x"),OpSo)
-    )), TmInt(2)),TmFunc("y",TmOp(TmIdent("y"),TmInt(2),OpSo));
+    )), TmNum(2)),TmFunc("y",TmOp(TmIdent("y"),TmNum(2),OpSo));
     
     (** Teste: {e/x}(let x:T => e1 in e2) -> let x:T {e/x}e1 in e2
       (fn x:T => (let x:T => x + 3 in x)) (2) **) // Valor do x em e2 só é procesado após substituição do x em e1
-    TmApp( TmFunc("x", TmLet("x",TmOp(TmIdent("x"),TmInt(3),OpSo),TmIdent("x"))), TmInt(2)),TmInt(5);
+    TmApp( TmFunc("x", TmLet("x",TmOp(TmIdent("x"),TmNum(3),OpSo),TmIdent("x"))), TmNum(2)),TmNum(5);
     
     (** Teste: {e/x}(let y:T => e1 in e2) -> let y:T {e/x}e1 in {e/x}e2
       (fn x:T => (let y:T => x + 3 in x)) (2) **) // x é livre em e2
-    TmApp( TmFunc("x", TmLet("y",TmOp(TmIdent("x"),TmInt(3),OpSo),TmIdent("x"))), TmInt(2)),TmInt(2);
+    TmApp( TmFunc("x", TmLet("y",TmOp(TmIdent("x"),TmNum(3),OpSo),TmIdent("x"))), TmNum(2)),TmNum(2);
 
     (** Teste: {e/f}(let rec f:T->T=(fn y:T => e1) in e2) -> (let rec f:T->T=(fn y:T => e1) in e2) 
     (fn f:T => (let rec f = fn x => x) (2) **) // f presso, nada é alterado. Valor 2 é desconsiderado
     TmApp( TmFunc("f", 
-              TmLetRec("f", TmFunc( "x", TmIdent("x") ),TmApp(TmIdent("f"),TmInt(1)))
-    ), TmInt(2)),TmInt(1);
+              TmLetRec("f", TmFunc( "x", TmIdent("x") ),TmApp(TmIdent("f"),TmNum(1)))
+    ), TmNum(2)),TmNum(1);
 
     // fatorial n-1
     (** Teste: {e/x}(let rec f:T->T=(fn y:T => e1) in e2 -> (let rec f:T->T={e/x}(fn y:T => e1) in {e/x}e2 
     let rec fat = fn y => if y-x=0 then 1 else y * fat(y-1) in fat(6-x) **) // aplica substituição quado x != f
     TmApp( TmFunc("x", 
             TmLetRec("f", TmFunc( "y", TmIf(
-                                      TmOp( TmIdent("y"),TmInt(0),OpIg ),
-                                      TmInt(1),
+                                      TmOp( TmIdent("y"),TmNum(0),OpIg ),
+                                      TmNum(1),
                                       TmOp( 
                                         TmIdent("y"),
-                                        TmApp( TmIdent("f"),TmOp( TmIdent("y"),TmInt(1),OpSu ) ),
+                                        TmApp( TmIdent("f"),TmOp( TmIdent("y"),TmNum(1),OpSu ) ),
                                         OpMu 
                                       )
-            )),TmApp(TmIdent("f"),TmOp( TmInt(6),TmIdent("x"),OpSu )))
-    ), TmInt(1)),TmInt(120);
+            )),TmApp(TmIdent("f"),TmOp( TmNum(6),TmIdent("x"),OpSu )))
+    ), TmNum(1)),TmNum(120);
     
   ]
   
